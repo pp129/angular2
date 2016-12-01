86a110624a0ef55c4f7560446048e290796638a2
panfei <604042454@qq.com>
2016-12-01 11:02:25 +0800

    null

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1 class="text-center">Hello {{name}}</h1>`,
+  template: `
+    <h1 class="text-center">Hello {{name}}</h1>
+  `,
 })
 export class AppComponent  { name = 'Angular'; }
